6d4c001da4f0921715cee5be42c7621542b1712d
Michael <segunmichael24@gmail.com>
2025-04-08 18:57:45 +0100

    Enhance SCAF functionality: add submit route for SCAF submissions, update student view to use form for submission, and clean up unused SCAR link in layout

--- routes/student.js
+++ routes/student.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const isAuthenticated = require('../middlewares/auth');
 const { getBankDetails, postBankDetails } = require('../controllers/student')
 const { getStudentForms, createForm8, submitForm8 } = require('../controllers/form8');
-const { getStudentSCAFs, createSCAF } = require('../controllers/scaf');
+const { getStudentSCAFs, createSCAF, submitSCAF } = require('../controllers/scaf');
 
 router.get('/bank-details', isAuthenticated, getBankDetails);
 router.post('/bank', isAuthenticated, postBankDetails);
 
 router.get('/scafs', isAuthenticated, getStudentSCAFs);
 router.post('/scafs', isAuthenticated, createSCAF);
+router.post('/scafs/:id/submit', isAuthenticated, submitSCAF);
+
 router.get('/scafs/new', isAuthenticated, (req, res) => res.render('student/scaf-new', {
     title: 'eBooklog - Fill SCAF Form',
     user: req.user
